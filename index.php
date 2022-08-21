ae9b0e7db05778b2b3080618e2162630b4d255fa
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2022-08-21 15:29:23 +0530

    /**new nav bar with a navigation panel db connection checked twice w3-css main selction with minor local css changes**/

--- index.php
+++ index.php
@@ -7,23 +7,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link type="image/png" sizes="96x96" rel="icon"
         href="https://img.icons8.com/external-soft-fill-juicy-fish/60/000000/external-appointment-online-services-soft-fill-soft-fill-juicy-fish.png">
+    <!-- basic html required -->
     <link rel="stylesheet" href="./css/main.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Home</title>
 </head>
 
 <body>
-    <div class="nav_bar">
-        <ul>
-            <!-- link change on main server after developement -->
-            <li><a class="active" href="index.php">Home</a></li>
-            <li><a href="account/login.php">Login</a></li>
-            <li><a href="about.php">About</a></li>
-        </ul>
+
+    <div class="w3-sidebar w3-bar-block" style="display:none" id="mySidebar">
+        <button onclick="w3_close()" class="w3-bar-item w3-button w3-large">&times;</button>
+        <a href="index.php" class="w3-bar-item w3-button">Home</a>
+        <a href="./account/login.php" class="w3-bar-item w3-button">Login</a>
+        <a href="./about.php" class="w3-bar-item w3-button">About</a>
     </div>
+    <!-- Page Content -->
+    <div class="">
+        <button class="w3-button w3-xlarge" onclick="w3_open()">☰</button>
+    </div>
+
     <div class="con_head">
         <p>Vela-patrak</p>
     </div>
+
+
     <script src="https://unpkg.com/scrollreveal"></script>
     <script src="./js/main.js"></script>
 </body>
