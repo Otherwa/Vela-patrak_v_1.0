1f23fbcdd3e21353cc3965a8898d81e158824c24
Pushkar <nzergaming@gmail.com>
2022-08-29 20:45:31 +0530

    /**Created course table in database with MemberId as foreign key**/

--- config/velapatrak.sql
+++ config/velapatrak.sql
@@ -3,15 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 27, 2022 at 05:22 PM
+-- Generation Time: Aug 29, 2022 at 05:14 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone
-= "+00:00";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -26,39 +24,41 @@ SET time_zone
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `CourseId` int(10) NOT NULL,
+  `CourseName` varchar(100) NOT NULL,
+  `Strength` int(10) NOT NULL,
+  `Abbrivation` varchar(100) NOT NULL,
+  `MemberId` int(11) NOT NULL,
+  `Date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `members`
 --
 
-CREATE TABLE `members`
-(
-  `MemberId` int
-(11) NOT NULL,
-  `FirstName` varchar
-(15) NOT NULL,
-  `LastName` varchar
-(15) NOT NULL,
-  `Department` varchar
-(10) NOT NULL,
-  `Email` varchar
-(25) NOT NULL,
-  `Phone` varchar
-(14) NOT NULL,
-  `Username` varchar
-(15) NOT NULL,
-  `Password` varchar
-(15) NOT NULL,
-  `Type` varchar
-(10) NOT NULL
+CREATE TABLE `members` (
+  `MemberId` int(11) NOT NULL,
+  `FirstName` varchar(15) NOT NULL,
+  `LastName` varchar(15) NOT NULL,
+  `Department` varchar(10) NOT NULL,
+  `Email` varchar(25) NOT NULL,
+  `Phone` varchar(14) NOT NULL,
+  `Username` varchar(15) NOT NULL,
+  `Password` varchar(15) NOT NULL,
+  `Type` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `members`
 --
 
-INSERT INTO `members` (`
-MemberId`,
-`FirstName
-`, `LastName`, `Department`, `Email`, `Phone`, `Username`, `Password`, `Type`) VALUES
+INSERT INTO `members` (`MemberId`, `FirstName`, `LastName`, `Department`, `Email`, `Phone`, `Username`, `Password`, `Type`) VALUES
 (1, 'Atharv', 'Desai', 'BScIT', 'atharvdesai2002@gmail.com', '8828388979', 'admin', 'timetable', 'admin'),
 (3, 'Pushkar', 'Sane', 'BSc IT', 'thekingpush417@gmail.com', '8355958447', 'admin', 'timetable', 'admin');
 
@@ -81,18 +81,16 @@ CREATE TABLE `timeslot` (
 --
 
 --
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD KEY `Member_course` (`MemberId`);
+
+--
 -- Indexes for table `members`
 --
 ALTER TABLE `members`
-ADD PRIMARY KEY
-(`MemberId`);
-
---
--- Indexes for table `timeslot`
---
-ALTER TABLE `timeslot`
-ADD KEY `Member_timeslot`
-(`MemberId`);
+  ADD PRIMARY KEY (`MemberId`);
 
 --
 -- Indexes for table `timeslot`
@@ -110,17 +108,21 @@ ALTER TABLE `timeslot`
 ALTER TABLE `members`
   MODIFY `MemberId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
-
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `course`
+--
+ALTER TABLE `course`
+  ADD CONSTRAINT `Member_course` FOREIGN KEY (`MemberId`) REFERENCES `members` (`MemberId`);
 
 --
 -- Constraints for table `timeslot`
 --
 ALTER TABLE `timeslot`
   ADD CONSTRAINT `Member_timeslot` FOREIGN KEY (`MemberId`) REFERENCES `members` (`MemberId`) ON DELETE CASCADE ON UPDATE CASCADE;
-
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
