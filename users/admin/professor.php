e5327e4c4a50e4daa4db81d0b76958d0a37926ee
Pushkar <nzergaming@gmail.com>
2022-09-07 16:00:06 +0530

    /**Initial commit for professor**/

--- users/admin/professor.php
+++ users/admin/professor.php
@@ -11,65 +11,6 @@ if (!isset($_SESSION['name'])) {
 // session passes id
 $id = $_SESSION['id'];
 
-// insert
-if (isset($_POST['save'])) {
-    $timeslot =  $_POST['TimeSlot'];
-    $starttime =  $_POST['StartTime'];
-    $memberid =  $id; //is member id of login generated at login
-    $endtime =  $_POST['EndTime'];
-
-    if ($timeslot == " " || $starttime == " " || $endtime == " " || $memberid == " ") {
-        echo '<script>alert(\'Kindly Fill the Form Correctly\');</script>';
-    } else {
-        $con = get_con();
-        $sql = "SELECT * FROM `timeslot` WHERE TimeSlot = '" . $timeslot . "'";
-
-        $result = mysqli_query($con, $sql);
-        $result_user_type = mysqli_fetch_array($result);
-        $row = mysqli_num_rows($result);
-
-        if ($row > 0) {
-            // check if timeslot already exists or not simple redirect to it
-            echo "<script>alert('Timeslot already exsists');</script>";
-            // close connection
-            mysqli_close($con);
-        } else {
-            insert_timeslot($timeslot, $starttime, $endtime, $memberid);
-        }
-    }
-}
-
-// insert in timeslot
-function insert_timeslot($timeslot, $starttime, $memberid, $endtime)
-{
-    $con = get_con();
-    $sql = "INSERT INTO `timeslot` (`TimeSlot`, `StartTime`, `EndTime`, `MemberId`, `Date`) VALUES ('$timeslot', '$starttime', '$memberid', '$endtime',current_timestamp());";
-    if ($con->query($sql) === TRUE) {
-        echo "<script>alert('Time-Slot Successfully Registered');</script>";
-    } else {
-        echo "<script>alert('Something went wrong.');</script>";
-    }
-    $con->close();
-}
-
-function timeslot()
-{
-    $con = get_con();
-    $con = get_con();
-    $sql = "SELECT * FROM timeslot";
-    $result = $con->query($sql);
-
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while ($row = $result->fetch_assoc()) {
-            echo "<li>" . "Timeslot: " . $row["TimeSlot"] . " -Start-Time: " . $row["StartTime"] . " -End-Time: " . $row["EndTime"] . " -Member-id: " . $row["MemberId"] . " &nbsp;&nbsp" . "<a style=\"color:#131352 \" href=\"action\\admin_timeslot_update.php\\?UpdateId=" . $row["TimeSlot"] . "\">Update</a></li>";
-        }
-    } else {
-        echo "No Timeslot";
-    }
-    $con->close();
-}
-ob_end_flush();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -82,7 +23,7 @@ ob_end_flush();
         href="https://img.icons8.com/external-soft-fill-juicy-fish/60/000000/external-appointment-online-services-soft-fill-soft-fill-juicy-fish.png">
     <!-- basic html required -->
     <link rel="stylesheet" href="../../css/main.css">
-    <link rel="stylesheet" href="../../css/timeslot.css">
+    <link rel="stylesheet" href="../../css/professor.css">
 
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -97,7 +38,7 @@ ob_end_flush();
         <a href="../admin_dashboard.php" class="w3-bar-item w3-button">Dashboard</a>
         <a href="register.php" class="w3-bar-item w3-button">Registration</a>
         <a href="timeslot.php" class="w3-bar-item w3-button w3-black">Time-Slot</a>
-        <a href="#" class="w3-bar-item w3-button">Admin Feature 1</a>
+        <a href="professor.php" class="w3-bar-item w3-button">Professor</a>
         <a href="#" class="w3-bar-item w3-button">Admin Feature 1</a>
         <a href="#" class="w3-bar-item w3-button">Admin Feature 1</a>
         <a href="#" class="w3-bar-item w3-button">Admin Feature 1</a>
@@ -113,14 +54,14 @@ ob_end_flush();
     </code>
 
     <div class="con_head">
-        <p> Time-Slot </p>
+        <p> Professors </p>
     </div>
 
     <div class="container">
         <div class="list">
-            <p style="float:left">Time-Slots</p>
+            <p style="float:left">Profesor</p>
             <div class="form  w3-margin w3-whitesmoke w3-bar-block" style="width:auto;height:38rem">
-                <?php timeslot(); ?>
+                
             </div>
         </div>
         <br>
