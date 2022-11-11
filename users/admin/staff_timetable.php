38d53b946f20918b5bf84c4aa34ef686ea5a27c9
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2022-11-11 18:39:32 +0530

    /staff -timetable database done/

--- users/admin/staff_timetable.php
+++ users/admin/staff_timetable.php
@@ -18,36 +18,116 @@ $id = $_SESSION['id'];
 
 // insert
 if (isset($_POST['save'])) {
-    $timeslot =  $_POST['TimeSlot'];
-    $starttime =  $_POST['StartTime'];
-    $memberid =  $id; //is member id of login generated at login
-    $endtime =  $_POST['EndTime'];
 }
 
-// insert in subjects
-
-
-
-// list the subjects in db
-function subjects()
+// gets room no from rooms
+function get_room()
 {
+    $con = get_con();
+    $sql = "SELECT DISTINCT RoomNo FROM rooms";
+    $result = $con->query($sql);
 
-    // boiler plate
-    // $con = get_con();
-    // $con = get_con();
-    // $sql = "SELECT * FROM timeslot";
-    // $result = $con->query($sql);
-
-    // if ($result->num_rows > 0) {
-    //     // output data of each row
-    //     while ($row = $result->fetch_assoc()) {
-    //         echo "<li>" . "Timeslot: " . $row["TimeSlot"] . " -Start-Time: " . $row["StartTime"] . " -End-Time: " . $row["EndTime"] . " -Member-id: " . $row["MemberId"] . " &nbsp;&nbsp" . "<a style=\"color:#131352 \" href=\"action\\admin_timeslot_update.php\\?UpdateId=" . $row["TimeSlot"] . "\">Update</a>" . "&nbsp;<a style=\"color:red \" href=\"action\\admin_timeslot_delete.php\\?DeleteId=" . $row["TimeSlot"] . "\">Delete</a></li>";
-    //     }
-    // } else {
-    //     echo "No Timeslot";
-    // }
-    // $con->close();
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value=\"" . $row["RoomNo"] . "\">" . $row["RoomNo"] . "</option>";
+        }
+    } else {
+        echo "None Added";
+    }
+    $con->close();
 }
+
+// gets time from timeslot
+function get_time()
+{
+    $con = get_con();
+    $sql = "SELECT DISTINCT TimeSlot,StartTime,EndTime FROM timeslot";
+    $result = $con->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value=\"" . $row["TimeSlot"] . "\">" . $row["StartTime"] . " - " . $row["EndTime"] . "</option>";
+        }
+    } else {
+        echo "None Added ";
+    }
+    $con->close();
+}
+
+
+// get class from subjects
+// keyword class
+function get_classs()
+{
+    $con = get_con();
+    $sql = "SELECT DISTINCT Class FROM subject";
+    $result = $con->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value=\"" . $row["Class"] . "\">" . $row["Class"] . "</option>";
+        }
+    } else {
+        echo "None Added";
+    }
+    $con->close();
+}
+
+function get_subjects()
+{
+    $con = get_con();
+    $sql = "SELECT DISTINCT SubjectName FROM subject";
+    $result = $con->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value=\"" . $row["SubjectName"] . "\">" . $row["SubjectName"] . "</option>";
+        }
+    } else {
+        echo "None Added";
+    }
+    $con->close();
+}
+
+// get timetable staff list
+function get_staff_list()
+{
+    $con = get_con();
+    $sql = "SELECT * FROM timetable";
+    $result = $con->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "<li>" . $row["RoomNo"] . " " . $row["TimeSlot"] . " etc" . "</li>";
+        }
+    } else {
+        echo "None Added";
+    }
+    $con->close();
+}
+
+function get_academic_year()
+{
+    $con = get_con();
+    $sql = "SELECT * FROM config";
+    $result = $con->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value=\"" . $row["AcademicYear"] . "\">" . $row["AcademicYear"]  . "</option>";
+        }
+    } else {
+        echo "None Added";
+    }
+    $con->close();
+}
+
 ob_end_flush();
 ?>
 <!DOCTYPE html>
@@ -84,7 +164,7 @@ ob_end_flush();
     </code>
 
     <div class="con_head">
-        <p> Select Subjects </p>
+        <p> Staff-Timetable </p>
     </div>
 
     <div class="container">
@@ -93,36 +173,127 @@ ob_end_flush();
                 <div class="context">
                     <img src="https://github.githubassets.com/images/mona-loading-dark.gif" alt="octo"
                         style="height:3rem">
-                    <p>Select Subjects</p>
+                    <p>Staff-Timetable</p>
                 </div>
                 <br>
                 <br>
-                <div class="form__div">
-                    <input type="number" class="form__input" name="TimeSlot" id="TimeSlot" placeholder="e.g xyz"
-                        autocomplete="off">
-                    <label for="" class="form__label">Time-Slot</label>
+                <div class="con">
+                    <!-- <?php $years = range(2002, strftime("%Y", time())); ?> -->
+                    <div class="form__div">
+                        <label for="Academic Year">Academic Year:</label>
+                        <select id="academic_year">
+                            <option value="--">--</option>
+                            <!-- <?php foreach ($years as $year) : ?>
+                            <option value="<?php echo $year; ?>"><?php echo $year; ?></option>
+                            <?php endforeach; ?> -->
+                            <?php get_academic_year(); ?>
+                        </select>
+                    </div>
+                    <br>
+                    <div class="form__div">
+                        <label for="Room">Room:</label>
+                        <select name="room" id="room">
+                            <option value="--">--</option>
+                            <!-- get fuction php -->
+                            <?php get_room(); ?>
+                        </select>
+                    </div>
+                    <br>
+                    <div class="form__div">
+                        <label for="Room">Timming:</label>
+                        <select name="timming" id="timming">
+                            <option value="--">--</option>
+                            <!-- get fuction php -->
+                            <?php get_time(); ?>
+                        </select>
+                    </div>
+                    <br>
+                    <div class="form__div">
+                        <label for="Class">Class:</label>
+                        <select name="class" id="class">
+                            <option value="--">--</option>
+                            <!-- get fuction php -->
+                            <?php get_classs(); ?>
+                        </select>
+                    </div>
+                    <br>
+                    <div class="form__div">
+                        <label for="Semester">Semester:</label>
+                        <select name="semester" id="semester">
+                            <option value="--">--</option>
+                            <!-- get fuction php -->
+                            <option value="I">I</option>
+                            <option value="II">II</option>
+                            <option value="III">III</option>
+                            <option value="IV">IV</option>
+                            <option value="V">V</option>
+                            <option value="VI">VI</option>
+                        </select>
+                    </div>
+                    <br>
+                    <div class="form__div">
+                        <label for="Division">Division:</label>
+                        <select name="division" id="division">
+                            <option value="--">--</option>
+                            <!-- get fuction php -->
+                            <option value="A">A</option>
+                            <option value="B">B</option>
+                            <option value="C">C</option>
+                            <option value="D">D</option>
+                            <option value="E">E</option>
+                            <option value="F">F</option>
+                        </select>
+                    </div>
+                    <br>
+                    <div class="form__div">
+                        <label for="Subject">Subject:</label>
+                        <select name="subject" id="subject">
+                            <option value="--">--</option>
+                            <!-- get fuction php -->
+                            <?php get_subjects(); ?>
+                        </select>
+                    </div>
+                    <br>
+                    <div class="form__div">
+                        <label for="Day">Day:</label>
+                        <select name="day" id="day">
+                            <option value="--">--</option>
+                            <!-- get fuction php -->
+                            <option value="Monday">Monday</option>
+                            <option value="Tuesday">Tuesday</option>
+                            <option value="Wednesday">Wednesday</option>
+                            <option value="Thursday">Thursday</option>
+                            <option value="Friday">Friday</option>
+                            <option value="Saturday">Saturday</option>
+                        </select>
+                    </div>
+                    <div class="form__div">
+                        <label for="Part">Part:</label>
+                        <select name="part" id="part">
+                            <option value="--">--</option>
+                            <option value="Junior">Junior</option>
+                            <option value="Degree">Degree</option>
+                        </select>
+                    </div>
+                    <div class="form__div">
+                        <label for="Sem">Sem:</label>
+                        <select name="sem" id="sem">
+                            <option value="--">--</option>
+                            <option value="both">both</option>
+                            <option value="odd">odd</option>
+                            <option value="even">even</option>
+                        </select>
+                    </div>
                 </div>
-                <br>
-                <div class="form__div">
-                    <input type="time" class="form__input" name="StartTime" id="StartTime" placeholder="e.g xyz@123"
-                        autocomplete="off">
-                    <label for="" class="form__label">Start-Time</label>
-                </div>
-                <br>
-                <div class="form__div">
-                    <input type="time" class="form__input" name="EndTime" id="EndTime"
-                        placeholder="e.g someone@gmail.com" autocomplete="off">
-                    <label for="" class="form__label">End-Time</label>
-                </div>
-                <br>
                 <input class="button-primary w3-button w3-border w3-hover-blue w3-round" type="submit" value="Save"
                     name="save" style="float:right">
             </form>
         </div>
         <br>
         <div class="list">
-            <p style="float:left">Select Subjects</p>
+            <p style="float:left">Staff-Timetable</p>
             <div id="_list" class="form  w3-margin w3-whitesmoke w3-bar-block">
+                <?php get_staff_list(); ?>
             </div>
         </div>
     </div>
