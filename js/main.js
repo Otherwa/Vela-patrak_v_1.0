cf2ed34688bdc0a546bb2c7f010b073aa63fcedb
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2022-11-18 14:51:13 +0530

    /minor changes/

--- js/main.js
+++ js/main.js
@@ -22,8 +22,8 @@ const options = {
     right: '82px', // default: '32px'
     left: 'unset', // default: 'unset'
     time: '0.5s', // default: '0.3s'
-    mixColor: '#fff', // default: '#fff'
-    backgroundColor: '#eee',  // default: '#fff'
+    mixColor: '#eee', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
     buttonColorDark: '#100f2c',  // default: '#100f2c'
     buttonColorLight: '#fff', // default: '#fff'
     saveInCookies: true, // default: true,
