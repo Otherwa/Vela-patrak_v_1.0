029299f6b3ef10f5d4a1a1d7f089ccbb18e619e7
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2022-11-23 17:49:58 +0530

    /major changes/

--- js/room_timetable.js
+++ js/room_timetable.js
@@ -3,13 +3,12 @@ function get_data_timetable(data) {
     console.log(data);
     let academic_year = $("#academic_year").val();
     let class1 = $("#class").val();
-    let room = $("#room").val();
-    let day = data;
+    let room = data;
     console.log(academic_year);
     $.ajax({
         type: 'post',
         url: 'adminajax.php',
-        data: "class202=" + class1 + "&ad6=" + academic_year + "&day=" + day + "&room=" + room,
+        data: "class202=" + class1 + "&ad6=" + academic_year + "&room=" + room,
         success: function (data) {
             $('#load_data').html(data);
 
